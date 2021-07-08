62cd417aeb840502d6ff5480d2947b7073d8ac01
Enigma-cloud <torreskristian77@gmail.com>
2021-07-08 15:36:15 +0100

    s: adding style to shapes dropdown

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const brushIcon = document.getElementById('brush');
 const brushSize = document.getElementById('brush-size');
 const brushSlider = document.getElementById('brush-slider');
 const bucketColorBtn = document.getElementById('bucket-color');
-const eraser = document.getElementById('eraser');
-// Undo/Redo
-const undoBtn = document.getElementById('undo-stroke');
-const redoBtn = document.getElementById('redo-stroke');
+// Create shapes
+const shapesBtn = document.getElementById('shapes');
 
+const eraser = document.getElementById('eraser');
 const clearCanvasBtn = document.getElementById('clear-canvas');
 const saveStorageBtn = document.getElementById('save-storage');
 const loadStorageBtn = document.getElementById('load-storage');
